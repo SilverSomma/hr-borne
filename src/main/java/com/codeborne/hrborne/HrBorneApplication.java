66b5e369d2f6188add1c2c73b690f6435ff36551
vali-it <asaas>
2022-07-18 11:42:17 +0300

    Initial commit

---
+++ src/main/java/com/codeborne/hrborne/HrBorneApplication.java
@@ -0,0 +1,13 @@
+package com.codeborne.hrborne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrBorneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrBorneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeborne/hrborne/HrBorneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeborne.hrborne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrBorneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
