ab6b8233369900015e5c4d0a0af8d56a8a26e61a
Silver Somma <silvertiit@gmail.com>
2022-07-18 16:57:29 +0300

    save to Absence repository

---
+++ src/main/java/com/codeborne/hrborne/AbsenceRepository.java
@@ -0,0 +1,37 @@
+package com.codeborne.hrborne;
+
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.stereotype.Service;
+
+import java.sql.Date;
+import java.util.Map;
+import java.util.UUID;
+
+@Service
+public class AbsenceRepository {
+
+    public static void main(String[] args) {
+        AbsenceLine absenceLine = new AbsenceLine(UUID.fromString("3488884c-120b-4542-8179-4d0c7194f24c"), "5e536974-54be-45fa-845f-33f38be7023a", Date.valueOf("2022-06-01"), Date.valueOf("2022-06-10"),"this is first line");
+        saveAbsence(absenceLine);
+
+    }
+
+    public static void saveAbsence(AbsenceLine absenceLine) {
+
+        NamedParameterJdbcTemplate jdbcTemplate = new NamedParameterJdbcTemplate(new DriverManagerDataSource
+                ("jdbc:postgresql://localhost:1234/postgres", "user", "pass"));
+
+        jdbcTemplate.update("INSERT INTO absences VALUES (:id, :userId, :absenceTypeId, :absenceStart, " +
+                ":absenceEnd, :description)", Map.of(
+                "id", UUID.randomUUID(),
+                "userId", absenceLine.getUserId(),
+                "absenceTypeId", UUID.fromString(absenceLine.getType()),
+                "absenceStart", absenceLine.getStart(),
+                "absenceEnd", absenceLine.getEnd(),
+                "description", absenceLine.getDescription()
+        ));
+
+    }
+
+}
---
+++ src/test/java/com/codeborne/hrborne/AbsenceRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.codeborne.hrborne;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AbsenceRepositoryTest {
+
+//    @Test
+//    void saveAbsence
+
+}
