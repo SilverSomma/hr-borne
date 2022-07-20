d13cc5e76ee070ffaa9cb8dc4bc0b2ff05b338c8
vali-it <asaas>
2022-07-20 11:19:27 +0300

    GOOGLE AUTH request generated and send

--- UI/src/views/LoginView.vue
+++ UI/src/views/LoginView.vue
@@ -14,7 +14,6 @@ export default {
       url: 'https://accounts.google.com/o/oauth2/auth?approval_prompt=force&scope=openid&client_id=27040378069-ikfaqni1p3kjuu3chbnrado9snib2usi.apps.googleusercontent.com&redirect_uri=http://localhost:8081/absences&response_type=code&access_type=offline&state=test' }
   },
 
-
   methods: {
     logIn: function () {
       fetch( this.url, {
@@ -22,30 +21,11 @@ export default {
         mode: 'no-cors',
       }
       ).then(response => {
-        window.location = 'https://accounts.google.com/o/oauth2/auth?approval_prompt=force&scope=openid&client_id=27040378069-ikfaqni1p3kjuu3chbnrado9snib2usi.apps.googleusercontent.com&redirect_uri=http://localhost:8081/absences&response_type=code&access_type=offline&state=test'
+        window.location = this.url
         console.log("AAA")
         console.log(response.data)
       }).catch(error => console.log(error))
 
-      // this.$http.get('https://accounts.google.com/o/oauth2/auth', {
-      //   // headers: {
-      //   //   'Access-Control-Allow-Origin': '*',
-      //   //   'Access-Control-Allow-Methods': '*',
-      //   //   'Access-Control-Allow-Headers': '*'
-      //   // },
-      //   params: {
-      //     approval_prompt: 'force',
-      //     scope: 'openid',
-      //     client_id: '1027130033615-5jhnjfo34a07p8omcqbsf9i3bjl75gqo.apps.googleusercontent.com',
-      //     redirect_uri: 'http://localhost:8081/absences',
-      //     response_type: 'code',
-      //     access_type: 'offline',
-      //     state: 'test'
-      //   },
-      //
-      // }).then(response => {
-      //   console.log(response.data)
-      // }).catch(error => console.log(error))
     }
   }
 }
